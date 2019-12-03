30f785177a57c3e786a7c3e1bdb50d4b51cd4732
Petri Airio <petri.airio@nordea.com>
2019-12-03 15:24:24 +0200

    Horo fetcher

--- src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/HoroCmd.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/HoroCmd.java
@@ -7,7 +7,6 @@ import org.freakz.hokan_ng_springboot.bot.common.events.InternalRequest;
 import org.freakz.hokan_ng_springboot.bot.common.events.ServiceRequestType;
 import org.freakz.hokan_ng_springboot.bot.common.events.ServiceResponse;
 import org.freakz.hokan_ng_springboot.bot.common.exception.HokanException;
-import org.freakz.hokan_ng_springboot.bot.common.models.HoroHolder;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -34,9 +33,9 @@ public class HoroCmd extends Cmd {
     public void handleRequest(InternalRequest request, EngineResponse response, JSAPResult results) throws HokanException {
         String horo = results.getString(ARG_HORO);
         ServiceResponse serviceResponse = doServicesRequest(ServiceRequestType.HORO_REQUEST, request.getIrcEvent(), horo);
-        HoroHolder hh = serviceResponse.getHoroResponse();
+        String hh = serviceResponse.getHoroResponse();
         if (hh != null) {
-            response.setResponseMessage(hh.toString());
+            response.setResponseMessage(hh);
         } else {
             response.setResponseMessage("Saat dildoo perään ja et pääse pylsimään!");
         }
