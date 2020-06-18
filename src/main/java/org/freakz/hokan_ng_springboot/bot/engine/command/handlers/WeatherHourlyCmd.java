0dd24dfb89b99d1ed4dcca28922da2e968aa70a7
Petri Airio <petri.j.airio@gmail.com>
2020-06-18 18:29:08 +0300

    back to ilmatieteenlaitos hourly

--- src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/WeatherHourlyCmd.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/WeatherHourlyCmd.java
@@ -16,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.freakz.hokan_ng_springboot.bot.common.util.StaticStrings.ARG_PLACE;
 
 /**
@@ -49,10 +52,85 @@ public class WeatherHourlyCmd extends Cmd {
     String place = results.getString(ARG_PLACE).toLowerCase();
     CityData cityData = cityResolver.resolveCityNames(place);
 
+    List<String> hoursL = new ArrayList<>();
+    List<String> tempsL = new ArrayList<>();
+
+    List<HourlyWeatherData> datas = new ArrayList<>();
     for (String city : cityData.getResolvedCityNames()) {
 
       ServiceResponse serviceResponse = doServicesRequest(ServiceRequestType.ILMATIETEENLAITOS_HOURLY_REQUEST, request.getIrcEvent(), city);
       HourlyWeatherData hourlyWeatherData = serviceResponse.getHourlyWeatherData();
+      if (hourlyWeatherData != null && hourlyWeatherData.getTimes() != null) {
+        datas.add(hourlyWeatherData);
+      }
+    }
+
+    if (datas.size() == 0) {
+      response.addResponse("Nothing found: %s!!", place);
+    } else {
+      if (datas.size() == 1) {
+        String city = cityData.getResolvedCityNames().get(0);
+        HourlyWeatherData hourlyWeatherData = datas.get(0);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hourly forecast: ");
+        sb.append(city);
+        sb.append("\n");
+        String hours = "";
+        String temps = "";
+        String format = getFormat(hourlyWeatherData.getLongestTemp());
+        for (int i = 0; i < hourlyWeatherData.getTimes().length; i++) {
+          hours += String.format(format, hourlyWeatherData.getTimes()[i]);
+          temps += String.format(format, hourlyWeatherData.getTemperatures()[i]);
+        }
+        hoursL.add(hours);
+        tempsL.add(temps);
+
+        sb.append(hours);
+        sb.append("\n");
+        sb.append(temps);
+        sb.append("\n");
+        response.addResponse("%s", sb.toString());
+
+      } else {
+        String cities = "";
+        String tempss = "";
+        String hourss = null;
+        for (int i = 0; i < datas.size(); i++) {
+          if (i > 0) {
+            cities += ", ";
+          }
+          cities += cityData.getResolvedCityNames().get(i);
+
+          HourlyWeatherData hourlyWeatherData = datas.get(i);
+          String hours = "";
+          String temps = "";
+
+          String format = getFormat(hourlyWeatherData.getLongestTemp());
+          for (int i2 = 0; i2 < hourlyWeatherData.getTimes().length; i2++) {
+            hours += String.format(format, hourlyWeatherData.getTimes()[i2]);
+            temps += String.format(format, hourlyWeatherData.getTemperatures()[i2]);
+          }
+          if (hourss == null) {
+            hourss = hours;
+          }
+          tempss = temps + "\n";
+
+        }
+        String resp = "Hourly forecast: " + cities + "\n";
+        resp += hourss + "\n";
+        resp += tempss + "\n";
+
+        response.addResponse(resp);
+      }
+    }
+
+
+/*
+      for (String city : cityData.getResolvedCityNames()) {
+
+      ServiceResponse serviceResponse = doServicesRequest(ServiceRequestType.ILMATIETEENLAITOS_HOURLY_REQUEST, request.getIrcEvent(), city);
+      HourlyWeatherData hourlyWeatherData = serviceResponse.getHourlyWeatherData();
+
 
       if (hourlyWeatherData != null && hourlyWeatherData.getTimes() != null) {
         StringBuilder sb = new StringBuilder();
@@ -66,6 +144,9 @@ public class WeatherHourlyCmd extends Cmd {
           hours += String.format(format, hourlyWeatherData.getTimes()[i]);
           temps += String.format(format, hourlyWeatherData.getTemperatures()[i]);
         }
+        hoursL.add(hours);
+        tempsL.add(temps);
+
         sb.append(hours);
         sb.append("\n");
         sb.append(temps);
@@ -78,7 +159,10 @@ public class WeatherHourlyCmd extends Cmd {
 
       }
     }
+    for (int i = 0; i < tempsL.size(); i++) {
 
+    }
+*/
   }
 
   private String getFormat(int longestTemp) {
