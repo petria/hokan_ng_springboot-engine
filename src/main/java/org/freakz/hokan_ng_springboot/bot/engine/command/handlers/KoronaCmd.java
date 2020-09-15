7f4de774d7dc8e96948d9e99774ce37ccc7fe11b
Petri Airio <petri.j.airio@gmail.com>
2020-09-15 16:49:13 +0300

    Jano / last *glugga*

--- src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/KoronaCmd.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/engine/command/handlers/KoronaCmd.java
@@ -8,37 +8,18 @@ import org.freakz.hokan_ng_springboot.bot.common.events.ServiceResponse;
 import org.freakz.hokan_ng_springboot.bot.common.exception.HokanException;
 import org.freakz.hokan_ng_springboot.bot.engine.command.HelpGroup;
 import org.freakz.hokan_ng_springboot.bot.engine.command.annotation.HelpGroups;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 @Scope("prototype")
 @HelpGroups(
-        helpGroups = {HelpGroup.SMS}
+        helpGroups = {HelpGroup.DATA_FETCHERS}
 )
 public class KoronaCmd extends Cmd {
 
     public KoronaCmd() {
         setHelp("Korona status!");
-    }
-
-    public String getKoronas() {
-        String title = "";
-        try {
-            String url = "https://korona.kans.io/";
-            Document doc = Jsoup.connect(url).get();
-            Elements body = doc.getElementsByTag("title");
-            return body.text();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     @Override
